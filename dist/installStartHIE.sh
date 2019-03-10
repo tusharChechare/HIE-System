5017f13bbae7e6579d348fd61e1b19093ae1a447
tusharChechare <47806489+tusharChechare@users.noreply.github.com>
2019-03-10 19:46:05 +0530

    Update installStartHIE.sh

--- dist/installStartHIE.sh
+++ dist/installStartHIE.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+echo "Enter version Number of BNA";
+read version
+echo "Installing the HIE-System";
+composer network install -a hie-system@$version.bna -c PeerAdmin@hlfv1
+echo "Successfully installed";
+echo "Starting HIE System";
+composer network start -c PeerAdmin@hlfv1 -n hie-system -V $version -A admin -S adminpw
+echo "Successfully Started";
